f3d1d04bd2977136c6d74934551fdfa106201d1d
AphexHenry <baptiste.bohelay@gmail.com>
2015-01-27 02:18:42 +0100

    Get all column names method added

---
+++ libs/dbWrapper.js
@@ -0,0 +1,34 @@
+
+var fs = require("fs");
+var sqlite3 = require("sqlite3").verbose();
+var db;
+exports.setup = function(aName, aCallback)
+{
+	var exists = fs.existsSync(aName);
+	if(!exists)
+	{
+		aCallback("no such file");
+	}
+	else
+	{
+		aCallback(null);
+		db = new sqlite3.Database(aName);
+	}
+}
+
+exports.getAllColumnNames = function(aCallback)
+{
+  	db.all("PRAGMA table_info(census_learn_sql);", function(err, data) 
+  	{
+	  	var rTable = [];
+	  	console.log(err);
+	  	for(var field in data)
+	  	{
+	  		if(data[field].name != 'age')
+	  		{
+	  			rTable.push({name:data[field].name});
+	  		}
+	  	}
+	  	aCallback(rTable);
+	 });
+}
--- test/tests.js
+++ test/tests.js
@@ -1,9 +1,12 @@
 var expect = require("chai").expect;
-var db = require("../lib/dbWrapper.js");
+var db = require("../libs/dbWrapper.js");
+var chai = require("chai");
+chai.should();
+chai.use(require('chai-things'));
 
 describe("dbWrapper", function(){
     describe("#setup()", function(){
-       it("should return that db us-census.db exist", function(done){
+       it("should return that valid db exist", function(done){
            var results = db.setup("us-census.db", function(err)
             {
               expect(err).to.equal(null);
@@ -16,6 +19,18 @@ describe("dbWrapper", function(){
               expect(err).to.not.equal(null);
               done();
             });
+       });
+   });
+
+    describe("#getAllColumnNames()", function(){
+       it("should return names of columns that are not 'age'", function(done){
+           db.getAllColumnNames(function(data)
+            {
+              console.log(data);
+              data.should.include.something.that.deep.equals({name:'occupation code'});
+              data.should.not.include.something.that.deep.equals({name:'age'});
+              done();
+            }); 
        });
    });
 });
