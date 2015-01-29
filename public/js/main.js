faf35d9fff13ab1792d2bcc59603a173f3bfefe0
AphexHenry <baptiste.bohelay@gmail.com>
2015-01-30 00:18:48 +0100

    Add loading icon and elements disable

--- public/js/main.js
+++ public/js/main.js
@@ -1,13 +1,20 @@
- var app = angular.module('MyApp', []);
+var app = angular.module('MyApp', []);
+// loading icon.
+var loadingSpinner;
 
+/*
+ *  Angular.
+ *  Manage requests and tables.
+ */
 angular.module("mainModule", [])
   .controller("InputController", function ($scope, $http)
   {
     $scope.fields = [{name:"loading datas..."}];
 
     $scope.data = {
-        name: "education",
+        name: "",
     };
+    $scope.loading = false;
 
     $scope.init = function () {
         // get the list of all available input fields in the db.
@@ -19,15 +26,23 @@ angular.module("mainModule", [])
 
     $scope.onChange = function ()
     {
+      loadingSpinner.spin($('body')[0]);
+      $scope.tableStyle = {opacity:'0.4'};
+      $scope.loading = true;
       var lData = angular.copy($scope.data);
+
       // get the table datas.
       $http.post("/getData", lData)
         .success(function (data, status, headers, config)
         {
+          $scope.loading = false;
+          $scope.tableStyle = {opacity:'1'};
+          loadingSpinner.stop();
           $scope.names = data;
         })
         .error(function (data, status, headers, config)
         {
+          $scope.loading = false;
           console.log("something else error");
         });
       }
@@ -35,9 +50,37 @@ angular.module("mainModule", [])
 
 
 jQuery(function($) {
+    SetupRowCountDisplay();
+    SetupSpinner();
+});
+
+/*
+ *  Will display the number of row not shown when reach the end of the scrolling table.
+ */
+function SetupRowCountDisplay()
+{
     $('#tableMainBody').bind('scroll', function() {
         if($(this).scrollTop() + $(this).innerHeight() >= this.scrollHeight) {
             
         }
+        else
+        {
+
+        }
     })
-});
+}
+
+/*
+ *  Setup the loading spinner.
+ */
+function SetupSpinner()
+{
+      var opts = {
+      radius: 5,
+      width: 2, // The line thickness
+      color: '#777', // #rgb or #rrggbb or array of colors
+  };
+
+  loadingSpinner = new Spinner(opts).spin($('#tableMainBody')[0]);
+  loadingSpinner.stop();
+}
